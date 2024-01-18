5354fdb7765f39493166d61b8e5cf9251a185249
DungPham2002 <phamdung260510@gmail.com>
2024-01-18 19:25:04 +0700

    post-archive

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PostRepository } from '../post/repositories/post.repository';
 import { RoomFilterDto } from './dto/room-filter.dto';
-import { Any, Between, ILike, In, IsNull, Like } from 'typeorm';
+import { Any, Between, ILike } from 'typeorm';
+import { PostEntity } from '../post/entities/post.entity';
 
 @Injectable()
 export class RoomService {
   constructor(private postRepo: PostRepository) {}
 
-  getAll() {
-    return this.postRepo.find();
+  async getAll(): Promise<PostEntity[]> {
+    return this.postRepo.find({
+      where: { published: true },
+      relations: ['user', 'comments'],
+    });
   }
 
-  getDetail(id: number) {
-    return this.postRepo.findOneBy({ id });
+  async getDetail(id: number): Promise<PostEntity | undefined> {
+    const zoom = await this.postRepo.findOne({
+      where: { id, published: true },
+      relations: ['user'],
+    });
+    if (!zoom) {
+      throw new BadRequestException('Can not found!');
+    }
+    return zoom;
   }
 
   async getAllWithFilter(request: RoomFilterDto) {
@@ -23,20 +34,21 @@ export class RoomService {
         order = { createdAt: 'DESC' };
         break;
       case 'LowToHigh':
-        order = { price: 'ASC', createdAt: 'DESC' };  
-      break;
+        order = { price: 'ASC', createdAt: 'DESC' };
+        break;
       case 'HighToLow':
         order = { price: 'DESC', createdAt: 'DESC' };
         break;
       default:
         order = { createdAt: 'ASC' };
     }
-    
+
     const rooms = await this.postRepo.find({
       where: {
         price: Between(request.search_price_min, request.search_price_max),
         area: Between(request.search_area_min, request.search_area_max),
         utilities: Any(ILike(`%${request.utilities}%`)),
+        published: true,
       },
       order: order,
     });
